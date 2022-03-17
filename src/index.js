6da7e39326b1dca9b01fe73942b5dd7296ff9ee3
ojack <ojackson@gmail.com>
2022-03-17 11:41:32 -0500

    added freesound library

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ const html = require('choo/html')
 const devtools = require('choo-devtools')
 const choo = require('choo')
 const store = require('./app/store.js')
+const mainView = require('./views/spirals.js')
 // const Fabric = require('./app/views/fabric.js')
 const CanvasExperiments = require('./app/views/draw-synth.js')
-const Hydra = require('./app/views/hydra-canvas.js')
-const Editor = require('./app/views/editor.js')
-const drawTools = require('./app/views/draw-tools.js')
+
 // const controls = require('./app/views/controls.js')
 const css = require('insert-css')
 const keymaps = require('./app/util/keymaps.js')
@@ -40,19 +39,4 @@ app.mount('#choo')
 //       ${state.cache(Editor, 'editor').render({ show: state.panels.editor})}
 //      ${controls(state, emit)}
 
-function mainView (state, emit) {
-  return html`
-    <div class="w-100 h-100 fixed absolute courier flex flex-column flex-row-ns" style="background-color:${state.style.color0};color:${state.style.color1}">
-      <div class="relative" style="flex-shrink:0;width:${state.style.width}px;height:${state.style.width}px">
-        <div class="absolute" id="hydra-container">${state.cache(Hydra, 'hydra').render({ width: state.style.width, height: state.style.width})}</div>
-        <div class=" absolute" id="fabric-container">${state.cache(CanvasExperiments, 'canvas-experiments').render({ width: state.style.width, height: state.style.width})}</div>
 
-      </div>
-      <div class="flex-auto">
-        ${drawTools(state, emit)}
-        ${state.cache(Editor, 'main-editor').render()}
-
-      </div>
-    </div>
-  `
-}
